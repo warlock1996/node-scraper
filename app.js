3ee12a249a66a9f4e286394a9c7fc296f878535d
warlock1996 <arslanali.921996@gmail.com>
2021-04-28 14:53:14 +0500

    :bug: port

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(bodyParser.json())
 app.get("/followers", getFollowers);
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("server running at http://localhost:3000");
 });
