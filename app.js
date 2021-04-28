72265c4179eeb7343e665638412ead7ff50563bc
warlock1996 <arslanali.921996@gmail.com>
2021-04-28 15:13:54 +0500

    :shirt: port config

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const { getFollowers } = require("./handler");
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.get("/followers", getFollowers);
 
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("server running at http://localhost:3000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server running ");
 });
